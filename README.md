eade7080f1a744501b7cfad6d4f6d9ee7b1184f5
jamiemarshfeay <jamiecfeay@hotmail.co.uk>
2025-10-16 14:47:32 +0100

    created tables

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+In order to connect to your databases, you'll need to set up their environments:
+
+- In the main directory of the repository add two files, one for the development database, and one for the test database.
+- Call the first file `.env.test` and the second `.env.development`.
+- Within `.env.test` enter the follwing: `PGDATABASE=nc_news_test`. Within `.env.development` enter the following: `PGDATABASE=nc_news`. The contents of these files should now allow to connect to both databases locally.
+- The `.gitignore` file should already include `.env.*` to ensure these environments are not pushed to git in future. If the file doesn't include `.env.*`, add it in.
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,104 @@
-const db = require("../connection")
+const format = require("node-pg-format");
+const {
+  topicData,
+  userData,
+  articleData,
+  commentData,
+} = require("../data/development-data/index.js");
+const db = require("../connection");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db.query(`
+    DROP TABLE IF EXISTS comments;
+    DROP TABLE IF EXISTS articles;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS topics;
+  `)
+  .then(() => {
+    return db.query(`
+      CREATE TABLE topics (
+        slug VARCHAR(255) PRIMARY KEY,
+        description VARCHAR(255),
+        img_url VARCHAR(1000)
+      );
+    `)
+  })
+  .then(() => {
+    return db.query(`
+      CREATE TABLE users (
+        username VARCHAR(255) PRIMARY KEY,
+        name VARCHAR(255),
+        avatar_url VARCHAR(1000)
+      );
+    `)
+  })
+  .then(() => {
+    return db.query(`
+      CREATE TABLE articles (
+        article_id SERIAL PRIMARY KEY,
+        title VARCHAR(255),
+        topic VARCHAR(255) REFERENCES topics(slug),
+        author VARCHAR(255) REFERENCES users(username),
+        body TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        votes INT DEFAULT 0,
+        article_img_url VARCHAR(1000)
+      );
+    `)
+  })
+  .then(() => {
+    return db.query(`
+      CREATE TABLE comments (
+        comment_id SERIAL PRIMARY KEY,
+        article_id INT REFERENCES articles(article_id),
+        body TEXT,
+        votes INT DEFAULT 0,
+        author VARCHAR(255) REFERENCES users(username),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `)
+  });
+    
+
+  // const nestedArrOfTopics = topicData.map((topic) => {
+  //   return [topic.slug, topic.description, topic.img_url];
+  // })
+
+  // const nestedArrOfUsers = userData.map((user) => {
+  //   return [user.username, user.name, user.avatar_url];
+  // })
+
+  // const nestedArrOfArticles = articleData.map((article) => {
+  //   return [article.article_id, article.title, article.topic, article.author, article.body, article.created_at, article.votes, article.article_img_url];
+  // })
+
+  // const nestedArrOfComments = commentData.map((comment) => {
+  //   return [comment.comment_id, comment.article_id, comment.body, comment.votes, comment.author, comment.created_at];
+  // })
+
+  // const topicsInsertStr = format(
+  //   `INSERT INTO topics (slug, description, img_url)
+  //     VALUES %L`,
+  //     nestedArrOfTopics
+  // );
+
+  // const usersInsertStr = format(
+  //   `INSERT INTO topics (username, name, avatar_url)
+  //     VALUES %L`,
+  //     nestedArrOfUsers
+  // );
+
+  // const articlesInsertStr = format(
+  //   `INSERT INTO topics (article_id, title, topic, author, body, created_at, votes, article_img_url)
+  //     VALUES %L`,
+  //     nestedArrOfArticles
+  // );
+
+  // const commentsInsertStr = format(
+  //   `INSERT INTO topics (comment_id, article_id, body, votes, author, created_at)
+  //     VALUES %L`,
+  //     nestedArrOfComments
+  // );
 };
+
 module.exports = seed;

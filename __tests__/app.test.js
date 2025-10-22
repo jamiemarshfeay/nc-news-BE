6f84b10af6e52ccb1d68c6b4b94979bc4217d923
jamiemarshfeay <jamiecfeay@hotmail.co.uk>
2025-10-22 17:14:00 +0100

    updated test suite - included 404 test for non-existent endpoint into every testing block

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,98 +13,148 @@ afterAll(() => {
 });
 
 describe("GET /api/topics", () => {
+  xtest("responds with a 404 status when passed a non-existent endpoint", () => {
+    return request(app)
+      .get("/api/notARoute")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Route not found");
+      });
+  });
   test("responds with a success status when connected to the api and accesses the topics", () => {
     return request(app)
-    .get("/api/topics")
-    .expect(200)
-    .then(({ body }) => {
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
         const topics = body.topics;
         expect(topics);
         topics.forEach((topic) => {
-            expect(typeof topic.slug).toBe("string");
-            expect(typeof topic.description).toBe("string");
+          expect(typeof topic.slug).toBe("string");
+          expect(typeof topic.description).toBe("string");
         });
-    });
+      });
   });
 });
 
 describe("GET /api/articles", () => {
+  xtest("responds with a 404 status when passed a non-existent endpoint", () => {
+    return request(app)
+      .get("/api/notARoute")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Route not found");
+      });
+  });
   test("responds with a success status when connected to the api and accesses the articles", () => {
     return request(app)
-    .get("/api/articles")
-    .expect(200)
-    .then(({ body }) => {
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
         const articles = body.articles;
         expect(articles);
         articles.forEach((article) => {
-            expect(typeof article.author).toBe("string");
-            expect(typeof article.title).toBe("string");
-            expect(typeof article.article_id).toBe("number");
-            expect(typeof article.topic).toBe("string");
-            expect(typeof article.created_at).toBe("string");
-            expect(typeof article.votes).toBe("number");
-            expect(typeof article.article_img_url).toBe("string");
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
         });
-    });
+      });
   });
   test("tests that each article has a comment count derived from the total number of comments with its respective ID", () => {
     return request(app)
-    .get("/api/articles")
-    .expect(200)
-    .then(({ body }) => {
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
         const articles = body.articles;
         articles.forEach((article) => {
-            expect(typeof article.comment_count).toBe("number");
+          expect(typeof article.comment_count).toBe("number");
         });
-    });
+      });
   });
   test("tests the articles are returned in descending date order", () => {
     return request(app)
-    .get("/api/articles")
-    .expect(200)
-    .then(({ body }) => {
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
         const articles = body.articles;
         for (let i = 1; i < articles.length; i++) {
-            expect(articles[i].created_at <= articles[i - 1].created_at).toBe(true);
+          expect(articles[i].created_at <= articles[i - 1].created_at).toBe(
+            true
+          );
         }
-    });
+      });
   });
   test("tests that there is no body property present on the articles", () => {
     return request(app)
-    .get("/api/articles")
-    .expect(200)
-    .then(({ body }) => {
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
         const articles = body.articles;
         articles.forEach((article) => {
-            expect(article.body).toBe(undefined);
+          expect(article.body).toBe(undefined);
         });
-    });
+      });
   });
 });
 
 describe("GET /api/users", () => {
+  xtest("responds with a 404 status when passed a non-existent endpoint", () => {
+    return request(app)
+      .get("/api/notARoute")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Route not found");
+      });
+  });
   test("responds with a success status when connected to the api and accesses the users", () => {
     return request(app)
-    .get("/api/users")
-    .expect(200)
-    .then(({ body }) => {
+      .get("/api/users")
+      .expect(200)
+      .then(({ body }) => {
         const users = body.users;
         expect(users);
         users.forEach((user) => {
-            expect(typeof user.username).toBe("string");
-            expect(typeof user.name).toBe("string");
-            expect(typeof user.avatar_url).toBe("string");
+          expect(typeof user.username).toBe("string");
+          expect(typeof user.name).toBe("string");
+          expect(typeof user.avatar_url).toBe("string");
         });
-    });
+      });
   });
 });
 
 xdescribe("GET /api/articles/:article_id", () => {
-  test("responds with a 200 status and accesses the article when passed a valid ID", () => {
+  xtest("responds with a 404 status when passed a non-existent endpoint", () => {
     return request(app)
-    .get("/api/articles/1")
-    .expect(200)
-    .then(({ body }) => {
+      .get("/api/notARoute")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Route not found");
+      });
+  });
+  xtest("responds with a 400 status when passed a completely invalid ID", () => {
+    return request(app)
+      .get("/api/articles/notAnId")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Invalid article ID");
+      });
+  });
+  xtest("responds with a 404 status when passed a valid possible ID, but one that does not exist", () => {
+    return request(app)
+      .get("/api/articles/100")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Article ID does not exist");
+      });
+  });
+  xtest("responds with a 200 status and accesses the article when passed a valid ID", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
         const article = body.article;
         expect(article.article_id).toBe(1);
         expect(typeof article.author).toBe("string");
@@ -114,14 +164,6 @@ xdescribe("GET /api/articles/:article_id", () => {
         expect(typeof article.created_at).toBe("string");
         expect(typeof article.votes).toBe("number");
         expect(typeof article.article_img_url).toBe("string");
-    });
-  });
-  xtest("responds with a 404 status when passed an invalid ID", () => {
-    return request(app)
-    .get("/api/articles/100")
-    .expect(404)
-    .then(({ body }) => {
-        expect(body.msg).toBe("Invalid article ID");
-    });
+      });
   });
 });

5aa7da2e02855f38191e16d42c272405aa838a99
jamiemarshfeay <jamiecfeay@hotmail.co.uk>
2025-10-27 14:58:59 +0000

    rearranged code within connection file in interest of hosting

--- db/connection.js
+++ db/connection.js
@@ -2,22 +2,23 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || 'development';
 
+require('dotenv').config({path: `${__dirname}/../.env.${ENV}`});
+
 const config = {};
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
-
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`});
-
+console.log(config)
 const db = new Pool(config);
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("No PGDATABASE or DATABASE_URL configured");
-} else { 
+} else if (process.env.PGDATABASE) { 
     console.log(`Connected to ${process.env.PGDATABASE}`);
+} else {
+    console.log("Connected to production database");
 }
-
 
 module.exports = db;

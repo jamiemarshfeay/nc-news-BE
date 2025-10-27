96fe2e13e37a36f34640367b5e2a58fdae5805c2
jamiemarshfeay <jamiecfeay@hotmail.co.uk>
2025-10-27 12:19:43 +0000

    made changes to connection, listen, and package.json files in the interest of hosting

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,22 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || 'development';
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+const config = {};
 
-const db = new Pool();
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
+require('dotenv').config({path: `${__dirname}/../.env.${ENV}`});
+
+const db = new Pool(config);
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("No PGDATABASE or DATABASE_URL configured");
 } else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+    console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
 
--- listen.js
+++ listen.js
@@ -1,9 +1,10 @@
 const app = require("./app.js");
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("Server is listening on port 9090...");
+    console.log(`Server is listening on port ${PORT}...`);
   }
 });
